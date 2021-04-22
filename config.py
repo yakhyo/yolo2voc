80788ce9662efafa334250c3db4a690405976fde
yakhyo <yakhyo9696@gmail.com>
2021-04-22 18:11:08 +0900

    Update files

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 label_dir = '..\\VOC2012\\JPEGLabels'
 image_dir = '..\\VOC2012\\JPEGImages'
-xml_dir   = '..\\VOC2012\\Annotations'
+xml_dir = '..\\VOC2012\\Annotations'
 
-names = [ 'aeroplane', 'bicycle', 'bird', 'boat',
-          'bottle', 'bus', 'car', 'cat', 'chair',
-          'cow', 'diningtable', 'dog', 'horse',
-          'motorbike', 'person', 'pottedplant',
-          'sheep', 'sofa', 'train', 'tvmonitor' ]
+names = ['aeroplane', 'bicycle', 'bird', 'boat',
+         'bottle', 'bus', 'car', 'cat', 'chair',
+         'cow', 'diningtable', 'dog', 'horse',
+         'motorbike', 'person', 'pottedplant',
+         'sheep', 'sofa', 'train', 'tvmonitor']
--- main.py
+++ main.py
@@ -1,9 +1,64 @@
-from xml.etree import ElementTree
 import os
 import config
+import argparse
+from PIL import Image
+import multiprocessing
+from xml.etree import ElementTree
+from pascal_voc_writer import Writer
 
 
-def voc2yolo_absolute(xml_file):
+def yolo2voc(txt_file):
+    w, h = Image.open(os.path.join(config.image_dir, f'{txt_file[:-4]}.jpg')).size
+    writer = Writer(f'{txt_file[:-4]}.xml', w, h)
+    with open(os.path.join(config.label_dir, txt_file)) as f:
+        for line in f.readlines():
+            label, x_center, y_center, width, height = line.rstrip().split(' ')
+            x_min = int(w * max(float(x_center) - float(width) / 2, 0))
+            x_max = int(w * min(float(x_center) + float(width) / 2, 1))
+            y_min = int(h * max(float(y_center) - float(height) / 2, 0))
+            y_max = int(h * min(float(y_center) + float(height) / 2, 1))
+            writer.addObject(config.names[int(label)], x_min, y_min, x_max, y_max)
+    writer.save(os.path.join(config.xml_dir, f'{txt_file[:-4]}.xml'))
+
+
+def voc2yolo(xml_file):
+    in_file = open(f'{config.xml_dir}/{xml_file}')
+    tree = ElementTree.parse(in_file)
+    size = tree.getroot().find('size')
+    h = int(size.find('height').text)
+    w = int(size.find('width').text)
+
+    class_exists = False
+    for obj in tree.findall('object'):
+        name = obj.find('name').text
+        if name in config.names:
+            class_exists = True
+
+    if class_exists:
+        out_file = open(f'{config.label_dir}/{xml_file[:-4]}.txt', 'w')
+        for obj in tree.findall('object'):
+            xml_box = obj.find('bndbox')
+            x_min = float(xml_box.find('xmin').text)
+            y_min = float(xml_box.find('ymin').text)
+            x_max = float(xml_box.find('xmax').text)
+            y_max = float(xml_box.find('ymax').text)
+
+            box_x_center = (x_min + x_max) / 2.0
+            box_y_center = (y_min + y_max) / 2.0
+            box_w = x_max - x_min
+            box_h = y_max - y_min
+            box_x_r = box_x_center * 1. / w
+            box_w_r = box_w * 1. / w
+            box_y_r = box_y_center * 1. / h
+            box_h_r = box_h * 1. / h
+            # _r means - relative
+
+            b = [box_x_r, box_y_r, box_w_r, box_h_r]
+            cls_id = config.names.index(obj.find('name').text)
+            out_file.write(str(cls_id) + " " + " ".join([str(f'{i:.6f}') for i in b]) + '\n')
+
+
+def voc2yolo_a(xml_file):
     in_file = open(f'{config.xml_dir}/{xml_file}')
     tree = ElementTree.parse(in_file)
 
@@ -22,16 +77,32 @@ def voc2yolo_absolute(xml_file):
             x_max = int(xml_box.find('xmax').text)
             y_max = int(xml_box.find('ymax').text)
 
-            bbox = [x_min, y_min, x_max, y_max]
+            b = [x_min, y_min, x_max, y_max]
             cls_id = config.names.index(obj.find('name').text)
-            out_file.write(str(cls_id) + " " + " ".join([str(f'{b}') for b in bbox]) + '\n')
+            out_file.write(str(cls_id) + " " + " ".join([str(f'{i}') for i in b]) + '\n')
         out_file.close()
 
 
 if __name__ == '__main__':
-    xml_files = [name for name in os.listdir(config.xml_dir) if name.endswith('.xml')]
-    for i in xml_files[:50]:
-        voc2yolo_absolute(i)
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument('--yolo2voc', action='store_true', help='YOLO to VOC')
+    parser.add_argument('--voc2yolo_a', action='store_true', help='VOC to YOLO absolute')
+    parser.add_argument('--voc2yolo_r', action='store_true', help='VOC to YOLO relative')
+    args = parser.parse_args()
 
+    if args.yolo2voc:
+        txt_files = [name for name in os.listdir(config.label_dir) if name.endswith('.txt')]
 
+        with multiprocessing.Pool(os.cpu_count()) as pool:
+            pool.map(yolo2voc, txt_files)
+
+    if args.voc2yolo:
+        xml_files = [name for name in os.listdir(config.xml_dir) if name.endswith('.xml')]
+        with multiprocessing.Pool(os.cpu_count()) as pool:
+            pool.map(voc2yolo, xml_files)
+
+    if args.voc2yolo_a:
+        xml_files = [name for name in os.listdir(config.xml_dir) if name.endswith('.xml')]
+        with multiprocessing.Pool(os.cpu_count()) as pool:
+            pool.map(voc2yolo_a, xml_files)
